73fab8a3ccf743ed6808ca424ed96ba510c4db08
mohanbalaji95 <mohanbalaji95@yahoo.com>
2018-05-22 18:11:32 -0700

    php to node exercise

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+//now processing post
+router.post('/storeData', function(req, res, next) {
+//expecting data variable called order--retrieve value using body-parser
+var value_name = req.body.order  //retrieve the data associated with order
+res.send("order succesfully received: " + value_name);
+});
+
+
+
+module.exports = router;
