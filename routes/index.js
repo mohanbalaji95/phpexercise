07910c9d5a8ab92d078823d9b7abffe8d5b74627
mohanbalaji95 <mohanbalaji95@yahoo.com>
2018-05-23 11:15:41 -0700

    mongo test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb:mbalaji:ammu9944@ds133360.mlab.com:33360/heroku_0sb1lwt6';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/mongodb', function(req, res, next) {
+    mongodb.MongoClient.connect(mongoDBURI, function (err, client) {
+        if (err) throw err;
+        //get handle to the database
+        var theDatabase = client.db('heroku_0sb1lwt6');
+
+
+        //get collection of Routes
+        var Routes = db.collection('Routes');
+        //get all Routes
+        Routes.find({}).sort({name: 1}).toArray(function (err, docs) {
+            if (err) throw err;
+
+            response.render('pages/mongodb', {results: docs});
+
+        });
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if (err) throw err;
+        });
+    });//mongoDb closed
+});//router closed
 
 
 
